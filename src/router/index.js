9b2d168367f8af2b1a63d4fc947726e2d211bcb5
Alvin SW <a.sebastian@connect.qut.edu.au>
2023-02-17 00:01:05 +1000

    Configure vue router to use createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import DescriboView from '../views/DescriboView.vue'
-
+//const base = document.querySelector('base').href;
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
