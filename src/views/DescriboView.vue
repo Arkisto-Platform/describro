e8fa2f5b4750d0688d63ee4ac7872287c807c852
Moises Sacal <moisbo@gmail.com>
2023-03-31 09:26:44 +1100

    minor styles changes

--- src/views/DescriboView.vue
+++ src/views/DescriboView.vue
@@ -68,17 +68,15 @@ const commands = {
       }
       data.crate = crate;
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   },
 
   async addFiles() {
-    data.loading = true;
     const crate = new ROCrate(data.crate);
     const dirHandle = data.dirHandle;
     await processFiles({crate, dirHandle, root: ''});
     data.crate = crate.toJSON();
-    data.loading = false;
   },
 
   async save() {
@@ -141,49 +139,49 @@ async function processFiles({crate, dirHandle, root}) {
 </script>
 
 <template>
-  <el-form :inline="true" class="bg-slate-200 p-2">
-    <el-form-item class="">
-      <el-dropdown trigger="click" @command="handleFileCommand">
-        <el-button type="primary">File &nbsp; <i class="fa-solid fa-caret-down"></i></el-button>
-        <template #dropdown>
-          <el-dropdown-menu>
-            <el-dropdown-item command="open">
-              <el-tooltip effect="dark" placement="right"
-                          content="Open a crate directory or an empty directory to create a new crate">
-                Open Directory
-              </el-tooltip>
-            </el-dropdown-item>
-            <el-dropdown-item command="addFiles">
-              <el-tooltip effect="dark" placement="right"
-                          content="Add Files">
-                Add Files
-              </el-tooltip>
-            </el-dropdown-item>
-            <el-dropdown-item command="save">
-              <el-tooltip effect="dark" placement="right"
-                          content="Save crate metadata to the currently opened directory">
-                Save
-              </el-tooltip>
-            </el-dropdown-item>
-          </el-dropdown-menu>
-        </template>
-      </el-dropdown>
-    </el-form-item>
-    <el-form-item label="Profile:" class="w-8/12">
-      <el-select v-model="data.selectedProfile" class="w-full" placeholder="Select" @change="changeProfile">
-        <el-option v-for="(profile, index) of profiles" :label="profile.metadata.name" :value="index" class="w-auto">
-          <span>{{ profile.metadata.name }}</span>
-          <span>{{ profile.metadata.description }}</span>
-        </el-option>
-      </el-select>
-    </el-form-item>
-    <div v-if="data.dirHandle" class="text-large font-600">{{ data.dirHandle.name }}</div>
-  </el-form>
-  <div class="describo" v-if="data.dirHandle" v-loading="data.loading">
-    <describo-crate-builder v-loading="data.loading" @ready="data.loading = false" @save:crate="saveCrate"
-                            :crate="data.crate" :profile="data.profile" :lookup="lookup">
-    </describo-crate-builder>
-  </div>
+    <el-form :inline="true" class="bg-slate-200 p-2">
+      <el-form-item class="">
+        <el-dropdown trigger="click" @command="handleFileCommand">
+          <el-button type="primary">File &nbsp; <i class="fa-solid fa-caret-down"></i></el-button>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item command="open">
+                <el-tooltip effect="dark" placement="right"
+                            content="Open a directory to describe or select an empty directory">
+                  Open Directory
+                </el-tooltip>
+              </el-dropdown-item>
+              <el-dropdown-item command="addFiles" :disabled="data.dirHandle?.name === undefined">
+                <el-tooltip effect="dark" placement="right"
+                            content="Load Files (With Selected Directory)">
+                  Load Files
+                </el-tooltip>
+              </el-dropdown-item>
+              <el-dropdown-item command="save">
+                <el-tooltip effect="dark" placement="right"
+                            content="Save crate metadata to the currently opened directory">
+                  Save Progress
+                </el-tooltip>
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </el-form-item>
+      <el-form-item label="Profile:" class="w-8/12">
+        <el-select v-model="data.selectedProfile" class="w-full" placeholder="Select" @change="changeProfile">
+          <el-option v-for="(profile, index) of profiles" :label="profile.metadata.name" :value="index" class="w-auto">
+            <span>{{ profile.metadata.name }}</span>
+            <span>{{ profile.metadata.description }}</span>
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <div v-if="data.dirHandle" class="text-large font-600">Selected Directory: {{ data.dirHandle.name }}</div>
+    </el-form>
+    <div class="describo" v-if="data.dirHandle">
+      <describo-crate-builder v-loading="data.loading" @ready="data.loading = false" @save:crate="saveCrate"
+                              :crate="data.crate" :profile="data.profile" :lookup="lookup">
+      </describo-crate-builder>
+    </div>
 </template>
 
 <style>
